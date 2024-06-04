995f8c8b8d2236fc1732712929ac12f945e94e59
jon-grant-state-ca-gov <jon.grant@state.ca.gov>
2024-06-03 21:56:38 -0700

    Version update and rebuild

--- components/header/build/index.asset.php
+++ components/header/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'bd8c713eb09a9326eb18');
+<?php return array('dependencies' => array(), 'version' => 'f64c9f2ae90a217a39a1');
--- components/header/build/index.js
+++ components/header/build/index.js
@@ -103,13 +103,11 @@ window.addEventListener("load", () => {
 /***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
 
 __webpack_require__.r(__webpack_exports__);
+/* eslint-disable yoda */
 window.addEventListener("load", () => {
   const doc = document.documentElement;
-  let prevScroll = window.scrollY || doc.scrollTop;
   let curScroll;
-  let direction = 0;
-  let prevDirection = 0;
-  const scrollNum = 40;
+  const prevScroll = window.scrollY || doc.scrollTop;
   const mainheader = document.querySelector("header");
   const navToggle = document.querySelector("header .nav-toggle");
   const navigation = document.querySelector("header .navigation");
@@ -122,6 +120,63 @@ window.addEventListener("load", () => {
   const navSearch = navigation.querySelector("li.search");
   const googleTranslate = navigation.querySelector("li.google-translate");
   const searchBox = document.getElementById("search-box");
+
+  // Set proper header and nav style on load
+  // Use compacted style if not at top of page
+  if (prevScroll !== 0) {
+    mainheader.classList.add("transparent-bg", "remove-box-shadow", "compacted");
+    logo.classList.add("hidden");
+    navigation?.classList.add("hidden");
+    caGovToggle.classList.add("hidden");
+  }
+  //If mobile
+  if (window.innerWidth < 1080) {
+    mainheader.classList.add("mobile", "gray-bg");
+    navigation?.classList.add("hidden");
+  }
+
+  // On resize, make sure to only lock menu scroll when CA gov menu or hamburger menu is open
+  const lockMenuScroll = () => {
+    if (sidebar.style.display === "block" || !navigation.classList.contains("hidden") && navigation.classList.contains("navigation-mobile")) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  };
+  const closeHamburgerMenu = () => {
+    document.body.style.overflow = "auto";
+    navigation.classList.add("hidden");
+    navToggle.ariaExpanded = "false";
+  };
+  const hideMobileHeader = () => {
+    mainheader.classList.remove("gray-bg");
+    mainheader.classList.add("transparent-bg", "remove-box-shadow");
+    logo.classList.add("hidden");
+    caGovToggle.classList.add("hidden");
+  };
+  const showMobileHeader = () => {
+    mainheader.classList.add("gray-bg");
+    mainheader.classList.remove("transparent-bg", "remove-box-shadow");
+    logo.classList.remove("hidden");
+    caGovToggle.classList.remove("hidden");
+  };
+  const hideDesktopHeader = () => {
+    navToggle.ariaExpanded = "false";
+    caGovToggle.classList.add("hidden");
+    mainheader.classList.add("transparent-bg", "remove-box-shadow", "compacted");
+    logo.classList.add("hidden");
+    navigation.classList.add("hidden");
+    sidebar.classList.add("sidebar-mobile");
+  };
+  const showDesktopHeader = () => {
+    navToggle.ariaExpanded = "false";
+    caGovToggle.classList.remove("hidden");
+    mainheader.classList.remove("transparent-bg", "remove-box-shadow", "compacted");
+    sidebar.classList.remove("sidebar-mobile");
+    logo.classList.remove("hidden");
+    caGovToggle.classList.remove("hidden");
+    navigation.classList.remove("hidden");
+  };
   if (!mainheader) return;
 
   // Escape key event listener
@@ -129,14 +184,12 @@ window.addEventListener("load", () => {
     if (navigation.classList.contains("navigation-mobile") && !navigation.classList.contains("hidden")) {
       if (e.key === "Escape") {
         e.stopPropagation();
-        document.body.style.overflow = "auto";
-        navigation.classList.add("hidden");
-        navToggle.ariaExpanded = "false";
+        closeHamburgerMenu();
       }
 
       // only if navToggle is focused and nav is open and tab key is pressed and shift is not pressed
       if (document.activeElement === navToggle && "true" === navToggle.ariaExpanded && e.key === "Tab" && e.shiftKey !== true) {
-        setTimeout(function () {
+        setTimeout(() => {
           // focus on the first element in the navigation
           navigation.querySelectorAll("input,a")[0].focus();
         }, 0);
@@ -144,9 +197,9 @@ window.addEventListener("load", () => {
     }
   });
 
-  // Close menu on focusout (tabbing out) event, desktop only
+  // Close menu on focusout (tabbing out) event, mobile layout only
   headerNav.addEventListener("focusout", e => {
-    if (!navigation.classList.contains("navigation-mobile") && !navigation.classList.contains("hidden")) {
+    if (navigation.classList.contains("navigation-mobile") && !navigation.classList.contains("hidden")) {
       const child = /** @type {Node} **/e.relatedTarget;
       const parent = /** @type {Node} **/e.currentTarget;
       if (child && !parent.contains(child) && "INPUT" !== child.nodeName) {
@@ -157,246 +210,141 @@ window.addEventListener("load", () => {
 
         // if tabbing out of the navigation and into the cagov sidebar
         if ("button" === child.localName) {
-          document.body.style.overflow = "auto";
-          navigation.classList.add("hidden");
-          navToggle.ariaExpanded = "false";
+          closeHamburgerMenu();
         }
       }
     }
   });
 
-  /* compacted header / hiding default header on scroll */
-  if (scrollNum < prevScroll && window.innerWidth > 1080) {
-    mainheader.classList.add("compacted", "transparent-bg", "remove-box-shadow");
-    caGovToggle.classList.add("hidden");
-    if (logo) {
-      logo.classList.add("hidden");
-    }
-    if (navigation) {
-      navigation.classList.add("hidden");
-    }
-  }
-
-  // Set proper header and nav style on load
-  if (window.innerWidth < 1080) {
-    mainheader.classList.add("mobile", "gray-bg");
-    if (navigation) {
-      navigation.classList.add("hidden");
-    }
-  } else {
-    mainheader.classList.remove("mobile", "gray-bg");
-    if (scrollNum > prevScroll && navigation) {
-      navigation.classList.remove("hidden");
-    }
-  }
-  window.addEventListener("scroll", () => {
-    /*
-     ** Find the direction of scroll
-     ** 0 - initial, 1 - up, 2 - down
-     */
-
-    curScroll = window.scrollY || doc.scrollTop;
-    // remove/add function
-    let logoFunc = "add";
-    let navFunc = "add";
-    if (curScroll > prevScroll) {
-      //scrolled up
-      direction = 2;
-    } else if (curScroll < prevScroll) {
-      //scrolled down
-      direction = 1;
-    }
-    if (direction !== prevDirection) {
-      // Mobile Only
-      if (window.innerWidth < 1080) {
-        // scrolling down
-        if (direction === 2 && curScroll > scrollNum) {
-          mainheader.classList.remove("gray-bg");
-          mainheader.classList.add("transparent-bg", "remove-box-shadow");
-          logoFunc = "add";
-          caGovToggle.classList.add("hidden");
-
-          // scrolling up
-        } else if (direction === 1 && curScroll < scrollNum) {
-          mainheader.classList.add("gray-bg");
-          mainheader.classList.remove("transparent-bg", "remove-box-shadow");
-          logoFunc = "remove";
-          caGovToggle.classList.remove("hidden");
-        }
-      } else {
-        // Desktop Only
-        // Toggle Header
-        // scrolling down
-        if (direction === 2 && curScroll > scrollNum) {
-          // Hide nav, logo, caGovToggle on scroll down
-          caGovToggle.classList.add("hidden");
-          mainheader.classList.add("transparent-bg", "remove-box-shadow", "compacted");
-          sidebar.classList.add("sidebar-mobile");
-          logoFunc = "add";
-          prevDirection = direction;
-          // scrolling up
-        } else if (direction === 1 && curScroll < scrollNum) {
-          if (window.innerWidth > 1080) {
-            // Desktop width
-            // Set hamburger menu icon to closed state
-            navToggle.ariaExpanded = "false";
-            caGovToggle.classList.remove("hidden");
-            mainheader.classList.remove("transparent-bg", "remove-box-shadow", "compacted");
-            navFunc = "remove";
-          }
-          sidebar.classList.remove("sidebar-mobile");
-          logoFunc = "remove";
-          prevDirection = direction;
-        }
-      }
-
-      // if logo exists
-      if (logo) {
-        logo.classList[logoFunc]("hidden");
-      }
-
-      // if navigation exists
-      if (navigation) {
-        navigation.classList[navFunc]("hidden");
-      }
-    }
-    prevScroll = curScroll;
-    if (direction === 1 && window.innerWidth > 1080) {
-      if (!mainheader.classList.contains("transparent-bg")) {
-        if (logo) {
-          logo.classList.remove("hidden");
-        }
-        if (navigation) {
-          navigation.classList.remove("hidden");
-        }
-      }
-    }
-  });
-
-  /* we also add the mobile class if screen is smaller than 1080px */
-  window.addEventListener("resize", () => {
-    // Was causing unexpected issues with clicking into search bar
-    // Set proper header and nav style on load
-    // if (window.innerWidth < 1080) {
-    //   mainheader.classList.add("mobile", "gray-bg");
-    //   if (navigation) {
-    //     navigation.classList.add("hidden");
-    //   }
-    // } else {
-    //   mainheader.classList.remove("mobile", "gray-bg");
-
-    //   if (scrollNum > prevScroll && navigation) {
-    //     navigation.classList.remove("hidden");
-    //   }
-    // }
-
-    // remove/add function
-    let searchFunc = "remove";
-
-    //  mobile only
-    if (window.innerWidth < 1080) {
-      mainheader.classList.add("mobile");
-      searchFunc = "add";
-      if (navigation) {
-        navigation.classList.add("navigation-mobile");
-      }
-      if (searchBox) {
-        searchBox.classList.add("focus-search-box");
-      }
-
-      // Was causing unexpected issues with clicking into search bar
-      // if (navSearch) {
-      //   headerNav.prepend(navSearch);
-      // }
-
-      // desktop only
-    } else if (window.innerWidth > 1080) {
-      mainheader.classList.remove("mobile");
-      searchFunc = "remove";
-      if (navSearch) {
-        headerNav.append(navSearch);
-      }
-      if (navigation) {
-        navigation.classList.remove("navigation-mobile");
-      }
-      if (googleTranslate) {
-        headerNav.append(googleTranslate);
-      }
-      if (scrollNum < prevScroll) {
-        mainheader.classList.remove("gray-bg");
-      } else if (navigation) {
-        navigation.classList.remove("hidden");
-      }
-    }
-    if (searchBox) {
-      searchBox.classList[searchFunc]("focus-search-box");
-    }
-  });
+  // On click listener
   if (navToggle) {
     navToggle.addEventListener("click", function () {
-      // remove/add function
-      let logoFunc = "remove";
-      let navFunc = "remove";
-
       // Hide cagov sidebar and show correct icon
       sidebar.style.display = "none";
       caGovLogo.classList.remove("ca-gov-close-icon");
       caGovLogo.classList.add("ca-gov-svg");
       caGovSvg.style.display = "block";
       this.ariaExpanded = this.ariaExpanded !== "true";
+
+      // Hamburger menu is open
       if ("true" === this.ariaExpanded) {
         mainheader.classList.remove("transparent-bg", "remove-box-shadow");
         caGovToggle.classList.remove("hidden");
-        logoFunc = "remove";
-        navFunc = "remove";
 
         // Mobile only
         if (window.innerWidth < 1080) {
           document.body.style.overflow = "hidden";
-          if (navigation) {
-            navigation.classList.add("navigation-mobile");
-          }
-          if (searchBox) {
-            searchBox.classList.add("focus-search-box");
-          }
+          logo?.classList.remove("hidden");
+          navigation?.classList.remove("hidden");
+          navigation?.classList.add("navigation-mobile");
+          searchBox?.classList.add("focus-search-box");
           if (navSearch) {
             headerNav.prepend(navSearch);
           }
+          // Desktop only
+        } else {
+          navigation?.classList.remove("hidden");
+          logo?.classList.remove("hidden");
         }
+
+        // Hamburger menu is closed
       } else {
         document.body.style.overflow = "auto";
-        navFunc = "add";
 
         // Desktop Only
         if (window.innerWidth > 1080) {
           mainheader.classList.add("transparent-bg", "remove-box-shadow");
+          navigation?.classList.add("hidden");
+          logo?.classList.add("hidden");
           caGovToggle.classList.add("hidden");
-          logoFunc = "add";
         }
 
-        // Mobile only & at top of screen
-        if (window.innerWidth < 1080 && curScroll > scrollNum) {
-          logoFunc = "add";
+        // Mobile only
+        if (window.innerWidth < 1080) {
           mainheader.classList.remove("gray-bg");
           mainheader.classList.add("transparent-bg", "remove-box-shadow");
+          navigation?.classList.add("hidden");
           caGovToggle.classList.add("hidden");
-          if (logo) {
-            logo.classList[logoFunc]("hidden");
+          logo?.classList.add("hidden");
+
+          // Top of screen
+          if (curScroll === 0) {
+            mainheader.classList.add("gray-bg");
+            mainheader.classList.remove("transparent-bg", "remove-box-shadow");
+            caGovToggle.classList.remove("hidden");
+            logo?.classList.remove("hidden");
           }
         }
       }
-
-      // if logo exists
-      if (logo) {
-        logo.classList[logoFunc]("hidden");
-      }
-
-      // if navigation exists
-      if (navigation) {
-        navigation.classList[navFunc]("hidden");
-      }
     });
   }
+  window.addEventListener("scroll", () => {
+    curScroll = window.scrollY || doc.scrollTop;
+
+    // Mobile Only
+    if (window.innerWidth < 1080) {
+      // Any scroll on mobile displays hamburger menu only
+      hideMobileHeader();
+
+      // Show at top of the page
+      if (curScroll === 0) {
+        showMobileHeader();
+      }
+    } else {
+      // Desktop Only
+      // Any scroll on desktop displays hamburger menu only
+      hideDesktopHeader();
+
+      // Show at top of the page
+      if (curScroll === 0) {
+        showDesktopHeader();
+      }
+    }
+  });
+
+  /* we also add the mobile class if screen is smaller than 1080px */
+  window.addEventListener("resize", () => {
+    curScroll = window.scrollY || doc.scrollTop;
+
+    // Was causing unexpected issues with clicking into search bar
+    // Checking if first child may prevent menu close on search box click
+    if (navSearch !== headerNav.firstElementChild) {
+      headerNav.prepend(navSearch);
+    }
+
+    //  mobile only
+    if (window.innerWidth < 1080) {
+      lockMenuScroll();
+      if (navToggle.ariaExpanded === "false") {
+        navigation?.classList.add("hidden");
+      }
+      mainheader.classList.add("mobile", "gray-bg");
+      mainheader.classList.remove("compacted");
+      navigation?.classList.add("navigation-mobile");
+      searchBox?.classList.add("focus-search-box");
+
+      // desktop only
+    } else {
+      // If CA Gov or hamburger menu are open, body overflow is hidden
+      lockMenuScroll();
+      mainheader.classList.remove("mobile");
+      searchBox?.classList.remove("focus-search-box");
+      navigation?.classList.remove("navigation-mobile");
+      if (navSearch) {
+        headerNav.append(navSearch);
+      }
+      if (googleTranslate) {
+        headerNav.append(googleTranslate);
+      }
+
+      // Not at top of the page
+      if (curScroll !== 0) {
+        mainheader.classList.add("compacted");
+      } else if (navigation) {
+        navigation.classList.remove("hidden");
+      }
+    }
+  });
 });
 
 /***/ }),
@@ -426,10 +374,6 @@ window.addEventListener("load", () => {
     if (window.innerWidth > 1080) {
       searchInput.classList.remove("focus-search-box");
     }
-  });
-  searchInput.addEventListener("touchend", e => {
-    e.preventDefault();
-    searchInput.focus();
   });
 });
 
